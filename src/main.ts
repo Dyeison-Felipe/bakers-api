670172d96fad3b1b5cca9255294c0b5d989f274d
dyeisonFK <dyeisonfc@gmail.com>
2026-08-21 18:08:21 -0300

    fix: realizado correção no arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
 
   console.log(`Server is running in port ${envConfig.getPort()}`)
 
-  await app.listen(envConfig.getPort() ?? '0.0.0.0');
+  await app.listen(envConfig.getPort(), '0.0.0.0');
 }
 bootstrap();
